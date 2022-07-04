52ba990f98c0b103ae2158dab55dc7f062e4d0ce
bachnnt <bachcvb@gmail.com>
2022-07-04 22:16:10 +0700

    menu

--- src/containers/System/Home/Home.js
+++ src/containers/System/Home/Home.js
@@ -10,23 +10,49 @@ import topTitle from '../../../assets/images/topTitle.png';
 class HomeComponent extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isDisplayMenuDetail: false,
+    }
   }
   componentDidMount() {
   }
   componentDidUpdate(prevProps, prevState, snapshot) {
   }
-
+  onFocusMenu = () => {
+    let { isDisplayMenuDetail } = this.state;
+    this.setState({
+      isDisplayMenuDetail: !isDisplayMenuDetail
+    });
+    console.log(isDisplayMenuDetail);
+  };
+  doClickMenu = () => {
+    this.setState({
+      isDisplayMenuDetail: false
+    });
+  }
   render() {
+    let { isDisplayMenuDetail } = this.state;
     return (
       <>
+        {isDisplayMenuDetail && <div className="menu">
+          <span className="close-menu-button" onClick={this.doClickMenu}>x</span>
+          <p>Home</p>
+          <p>Vision</p>
+          <p>DevilSilly World</p>
+          <p>Tokenomics</p>
+          <p>Roadmap</p>
+          <p>Our team</p>
+          <p>Partner</p>
+          <p>Technology</p>
+        </div>}
         <div className="home-boundary">
           <div className="top">
-            <img className="icon" src={icon} />
-            <img className="logo-game" src={logoGame} />
+            <img className="icon" onClick={this.onFocusMenu} src={icon} alt="iconMenu" />
+            <img className="logo-game" src={logoGame} alt="logoGame" />
             <span className="login">Login</span>
           </div>
           <div className="center">
-            <img className="background" src={background} />
+            <img className="background" src={background} alt="background" />
           </div>
           <div className="end">
             <div className="left-end">
